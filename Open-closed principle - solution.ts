f650fb54ee73381e54ef481131aec19b3c1dfc15
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2024-04-25 23:09:06 +0330

    Update Open-closed principle - solution.ts

--- Open-closed principle - solution.ts
+++ Open-closed principle - solution.ts
@@ -1,1 +1,38 @@
+interface ShapeAreaInterface {
+  getArea(): number;
+}
 
+class Triangle implements ShapeAreaInterface {
+  public base: number;
+  public height: number;
+  constructor(base: number, height: number) {
+    this.base = base;
+    this.height = height;
+  }
+  public getArea() {
+    return this.base * this.height * 0.5
+  }
+}
+
+class Rectangle implements ShapeAreaInterface {
+  public width: number;
+  public height: number;
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+  public getArea() {
+    return this.width * this.height;
+  }
+}
+
+function computeAreasOfShapes(
+  shapes: Shape[]
+) {
+  return shapes.reduce(
+    (computedArea, shape) => {
+      return computedArea + shape.getArea();
+    },
+    0
+  );
+}
