a897e4df24963cb54424631696a7bb4fa550e570
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2024-04-25 23:04:33 +0330

    Create Single responsibility principle - solution.ts

---
+++ Single responsibility principle - solution.ts
@@ -0,0 +1,19 @@
+class StudentAccount {
+  public createStudentAccount(){
+    // some logic
+  }
+}
+
+
+class StudentGrade {
+  public calculateStudentGrade(){
+    // some logic
+  }
+}
+
+
+class StudentData {
+  public generateStudentData(){
+    // some logic
+  }
+}
