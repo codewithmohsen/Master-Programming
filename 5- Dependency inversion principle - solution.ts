db02ce9968e9b779cddf3757e19fb550d742affc
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2024-04-25 23:25:15 +0330

    Update 5- Dependency inversion principle - solution.ts

--- 5- Dependency inversion principle - solution.ts
+++ 5- Dependency inversion principle - solution.ts
@@ -1,1 +1,23 @@
+interface Database {
+  create(order: Order): void;
+  update(order: Order): void;
+}
 
+class OrderService {
+  database: Database;
+  public create(order: Order): void {
+    this.database.create(order);
+  }
+  public update(order: Order): void {
+    this.database.update(order);
+  }
+}
+
+class MySQLDatabase implements Database {
+  public create(order: Order) {
+    // create and insert to database
+  }
+  public update(order: Order) {
+    // update database
+  }
+}
