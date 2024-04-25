945327c3db560cfe85cd380cc4279abdf31b4a9a
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2024-04-25 23:05:20 +0330

    Create Open-closed principle - problem.ts

---
+++ Open-closed principle - problem.ts
@@ -0,0 +1,18 @@
+class Triangle {
+  public base: number;
+  public height: number;
+  constructor(base: number, height: number) {
+    this.base = base;
+    this.height = height;
+  }
+}
+
+
+class Rectangle {
+  public width: number;
+  public height: number;
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+}
