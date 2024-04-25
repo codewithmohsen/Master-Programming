cb728d251bb06776538660e974bd735d031de42d
Mohsen Karimian (Hajikarimian) <135535893+codewithmohsen@users.noreply.github.com>
2024-04-25 23:04:01 +0330

    Update and rename Single responsibility principle.ts to Single responsibility principle - problem.ts

---
+++ Single responsibility principle - problem.ts
@@ -0,0 +1,13 @@
+class Student {
+  public createStudentAccount(){
+    // some logic
+  }
+
+  public calculateStudentGrade(){
+    // some logic
+  }
+
+  public generateStudentData(){
+    // some logic
+  }
+}
